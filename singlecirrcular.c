b9a6d87d4c06b4c2fd8d0c8a2b910d3d9ea40083
Rover <rover31b@gmail.com>
2022-03-05 20:00:01 +0530

    single circular

--- singlecirrcular.c
+++ singlecirrcular.c
@@ -1,114 +1,160 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct node{
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
     int data;
-    struct node * link;
+    struct node *link;
 };
-typedef struct node * Node;
-Node createnode(){
-    Node nn=(Node)malloc(sizeof(struct node));
+typedef struct node *Node;
+Node createnode()
+{
+    Node nn = (Node)malloc(sizeof(struct node));
     printf("Enter the data for the new node\n");
-    scanf("%d",&nn->data);
-    nn->link=NULL;
+    scanf("%d", &nn->data);
+    nn->link = NULL;
     return nn;
 }
-void insertrear(Node Head){
-    Node nn=createnode();
-    if(Head->link==Head){
-        Head->link=nn->link=nn;
+void insertrear(Node Head)
+{
+    Node nn = createnode();
+    if (Head->link == Head)
+    {
+        Head->link = nn->link = nn;
     }
-    else{
-        nn->link=Head->link->link;
-        Head->link->link=nn;
-        Head->link=nn;
+    else
+    {
+        nn->link = Head->link->link;
+        Head->link->link = nn;
+        Head->link = nn;
     }
     Head->data++;
 }
-void display(Node Head){
-    if(Head->data==0){
+void display(Node Head)
+{
+    if (Head->data == 0)
+    {
         printf("Nothing is here to display \n");
         return;
     }
-    Node first=Head->link->link;
-    do{
-        printf("%d\t",first->data);
-        first=first->link;
-    }while(first!=Head->link->link);
+    Node first = Head->link->link;
+    do
+    {
+        printf("%d\t", first->data);
+        first = first->link;
+    } while (first != Head->link->link);
     printf("\n");
 }
-void insertfront(Node Head){
-    Node nn=createnode();
-    if(Head->data==0){
-        nn->link=nn;
-        Head->link=nn;
+void insertfront(Node Head)
+{
+    Node nn = createnode();
+    if (Head->data == 0)
+    {
+        nn->link = nn;
+        Head->link = nn;
     }
-    else{
-        nn->link=Head->link->link;
-        Head->link->link=nn;
+    else
+    {
+        nn->link = Head->link->link;
+        Head->link->link = nn;
     }
     Head->data++;
 }
-void insertpos(Node head){
+void insertpos(Node head)
+{
     int pos;
     printf("ENter the pos\n");
-    scanf("%d",&pos);
-    if(pos>head->data+1|| pos<0){
+    scanf("%d", &pos);
+    if (pos > head->data + 1 || pos < 0)
+    {
         printf("Invalid pos\n");
         return;
     }
-    int count=1;
-    Node nn=createnode();
-    Node start=head->link->link;
-    while(count<pos-1){
-        start=start->link;
+    int count = 1;
+    Node nn = createnode();
+    Node start = head->link->link;
+    while (count < pos - 1)
+    {
+        start = start->link;
         count++;
     }
-    if(pos==1){
-        if(head->data==0){
-            nn->link=nn;
-            head->link=nn;
+    if (pos == 1)
+    {
+        if (head->data == 0)
+        {
+            nn->link = nn;
+            head->link = nn;
             head->data++;
             return;
         }
-        nn->link=start;
-        head->link->link=nn;
+        nn->link = start;
+        head->link->link = nn;
     }
-    else{
-    nn->link=start->link;
-    start->link=nn;
-    if(pos==head->data+1){
-        head->link=nn;
-    }
+    else
+    {
+        nn->link = start->link;
+        start->link = nn;
+        if (pos == head->data + 1)
+        {
+            head->link = nn;
+        }
     }
     head->data++;
-    
 }
-int main(){
-    Node Head=(Node)malloc(sizeof(struct node));
-    Head->data=0;
-    Head->link=Head;
-    int cnt=1;
+void deletebypos(Node Head)
+{
+    int pos;
+    printf("ENter the pos\n");
+    scanf("%d", &pos);
+    if (pos < 1 || pos > Head->data)
+    {
+        printf("Not possible \n");
+        return;
+    }
+    Node start = Head->link->link;
+    if (pos == 1)
+    {
+            if(Head->data==1){
+                Head->data--;
+                free(start);
+                return;
+            }
+        Head->link->link=start->link;
+        free(start);
+        Head->data--;
+    }
+    else{
+        
+    }
+}
+int main()
+{
+    Node Head = (Node)malloc(sizeof(struct node));
+    Head->data = 0;
+    Head->link = Head;
+    int cnt = 1;
     int choice;
-    while(cnt){
+    while (cnt)
+    {
         printf("Enter your choice\n");
         printf("1.Insert rear\n");
         printf("2.display\n");
         printf("3.Insert front\n");
         printf("Insert at pos\n");
-        scanf("%d",&choice);
-        switch(choice){
-            case 1:
-                insertrear(Head);
-                break;
-            case 2:
-                display(Head);
-                break;
-            case 3:
-                insertfront(Head);
-                break;
-            case 4:
-                insertpos(Head);
-                break;
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            insertrear(Head);
+            break;
+        case 2:
+            display(Head);
+            break;
+        case 3:
+            insertfront(Head);
+            break;
+        case 4:
+            insertpos(Head);
+            break;
         }
     }
 }
